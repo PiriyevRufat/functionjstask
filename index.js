8468f68feb3b4ad72676fe1c568b29c560f9aeb5
PiriyevRufat <tu7pj36at@code.edu.az>
2024-04-05 22:37:39 +0400

    function task done

---
+++ index.js
@@ -0,0 +1,41 @@
+// function monthlyPaymentFunc(payMoney, monthCount) {
+//     var monthlyPay = [];
+//     var herAyOdenilecekMiqdar = payMoney / monthCount;
+//     for (var i = 0; i < monthCount; i++) {
+//         monthlyPay.push(herAyOdenilecekMiqdar);
+//     }
+//     return monthlyPay;
+// }
+// var payMoney = 1212;
+// var monthCount = 3;
+
+// var monthlyPay = monthlyPaymentFunc(payMoney, monthCount);
+// console.log(monthlyPay);
+
+//task1
+
+// function findDuplicate(arr){
+//     let duplicates=[];
+//     for (let i = 0; i < arr.length - 1; i++){
+//        for (let j = i+1; j < arr.length; j++) {
+//         if (arr[i] === arr[j]) {
+//             duplicates.push(arr[i]);
+//         }
+//        }
+//     }
+//     return duplicates;
+// }
+
+// arr=[1,2,3,4,5,8,6,9,5]
+// console.log(findDuplicate(arr));
+
+//task2
+
+// function fahrenheitToCelsius(fahrenheit) {
+//     return (fahrenheit - 32) * 5 / 9;
+// }
+// let addfahrenheit=parseInt(prompt("Temperaturu Farengeytde daxil edin"))
+// let celsium= fahrenheitToCelsius(addfahrenheit)
+// console.log(celsium);
+
+// task3
